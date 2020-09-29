5bae2bde245361b934e85aa7a6a1b4e0f9d22005
Bernardo A. Dal Corno (c082647) <c082647@mail.caixa>
2020-09-29 17:40:13 -0300

    ToDo

---
+++ index.js
@@ -0,0 +1,11 @@
+#! /usr/bin/env node
+
+// check for updates
+// get link of download
+// download it
+// get path of vscode
+// take data one level up (rename to vscode_data_tmp)
+// unzip download
+// del current version
+// rename new version to same name pattern
+// move 
